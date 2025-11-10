0193d4e87ba41c52c8590e7b43590957145eda8a
panfiloow <panfiloow@mail.ru>
2025-11-10 17:18:57 +0300

    Настроен alembic

---
+++ app/database/session.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from app.core.config import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
+AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def get_async_session():
+    async with AsyncSessionLocal() as session:
+        yield session
+        
---
+++ app/models/user.py
@@ -0,0 +1,10 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from app.database.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(100))

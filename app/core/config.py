3788c8cae52a288a7d2c1e6f7c727569e99ae72e
panfiloow <panfiloow@mail.ru>
2025-11-10 16:27:01 +0300

    Настроен config и docker-compose для postgres

--- app/core/config.py
+++ app/core/config.py
@@ -1,21 +1,34 @@
-from typing import Optional
+from pydantic import computed_field
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-
     # Основные настройки
-    VERSION : str = "1.0.0"
-    API_V1_STR: str = "/api/v1"
+    VERSION : str
+    API_V1_STR: str
 
-    # Настройки базы данных
-    DATABASE_URL: Optional[str] = None
+    #Настройки базы данных
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_PORT: int
+    POSTGRES_HOST: str
+    
+    @computed_field
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
 
-    # JWT настройки
-    SECRET_KEY: str = "your-secret-key-change-in-production"
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    #JWT настройки
+    SECRET_KEY: str
+    REFRESH_SECRET_KEY: str
+    ALGORITHM: str 
+    ACCESS_TOKEN_EXPIRE_MINUTES: int 
+    REFRESH_TOKEN_EXPIRE_DAYS: int
 
     class Config:
         env_file = ".env"
 
 settings = Settings()
+
+if __name__ == "__main__":
+    print(settings)
